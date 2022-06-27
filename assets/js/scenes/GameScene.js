3a076cbe1c5d2902ea376f19fc0e8906ec09dfa7
sikgyu <57619483+sikgyu@users.noreply.github.com>
2022-06-27 16:13:37 -0700

    :art: Create a Title

--- assets/js/scenes/GameScene.js
+++ assets/js/scenes/GameScene.js
@@ -11,16 +11,13 @@ class GameScene extends Phaser.Scene {
         button.setOrigin(0.5, 0.5);
     
         this.add.sprite(300, 100, 'button1');
-    
-        this.chest = this.physics.add.image(300, 300, 'items', 0);
-    
         this.wall = this.physics.add.image(500, 100, 'button1', 0);
         this.wall.setImmovable();
+        
+        this.chest = new Chest(this, 300, 300, 'items', 0);
         // create character game object
-        this.player = this.physics.add.image(32, 32, 'characters', 0);
-        this.player.setScale(2); 
-        this.player.body.setCollideWorldBounds(true);
-    
+        this.player = new Player(this, 32, 32, 'characters', 0);
+
         // adding a collider between player and button for (testing)
         this.physics.add.collider(this.player, this.wall);
         this.physics.add.overlap(this.player, this.chest, function(player, chest) { goldPickupAudio.play(); chest.destroy(); }, null, this);
@@ -29,19 +26,7 @@ class GameScene extends Phaser.Scene {
         this.cursors = this.input.keyboard.createCursorKeys();
     }
 
-    update () {
-        this.player.setVelocity(0);
-    
-        if (this.cursors.left.isDown) {
-            this.player.setVelocityX(-160);
-        } else if (this.cursors.right.isDown) {
-            this.player.setVelocityX(160);
-        } 
-    
-        if (this.cursors.up.isDown) {
-            this.player.setVelocityY(-160);
-        } else if (this.cursors.down.isDown) {
-            this.player.setVelocityY(160);
-        }
+    update() {
+        this.player.update(this.cursors);
     }
 }
--- assets/js/scenes/TitleScene.js
+++ assets/js/scenes/TitleScene.js
@@ -4,11 +4,24 @@ class TitleScene extends Phaser.Scene {
         super('Title');
     }
 
-    preload() {
-
-    }
-
     create () {
-        
+        // create title text
+        this.titleText = this.add.text(this.scale.width / 2 , this.scale.height / 2 , 'Little Multiplayer Online RPG', { fontSize: '40px', fill: '#fff', fontFamily: 'Arial' });
+        this.titleText.setOrigin(0.5);
+        this.button = this.add.image(this.scale.width / 2 , this.scale.height * 0.65, 'button1');
+        this.button.setInteractive();
+        // create a Button
+        this.buttonText = this.add.text(0, 0, 'Start', { fontSize: '26px', fill: '#fff', fontFamily: 'Arial' });
+        Phaser.Display.Align.In.Center(this.buttonText, this.button);
+
+        this.button.on('pointerdown', () => {
+            this.scene.start('Game');
+        })
+        this.button.on('pointerover', () => {
+            this.button.setTexture('button2');
+        })
+        this.button.on('pointerout', () => {
+            this.button.setTexture('button1');
+        })
     }
 }
