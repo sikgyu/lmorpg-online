94071d724715122f69446c6327ac28d003237082
sikgyu <57619483+sikgyu@users.noreply.github.com>
2022-06-27 15:11:50 -0700

    :art: transition between scenes

--- assets/js/scenes/BootScene.js
+++ assets/js/scenes/BootScene.js
@@ -11,44 +11,7 @@ class BootScene extends Phaser.Scene {
         this.load.audio('goldsound', 'assets/audio/Pickup.wav')
     }
 
-    create () {
-        let goldPickupAudio = this.sound.add('goldsound', {loop: false, volume: 0.5});
-
-        let button = this.add.image(100, 100, 'button1');
-        button.setOrigin(0.5, 0.5);
-    
-        this.add.sprite(300, 100, 'button1');
-    
-        this.chest = this.physics.add.image(300, 300, 'items', 0);
-    
-        this.wall = this.physics.add.image(500, 100, 'button1', 0);
-        this.wall.setImmovable();
-        // create character game object
-        this.player = this.physics.add.image(32, 32, 'characters', 0);
-        this.player.setScale(2); 
-        this.player.body.setCollideWorldBounds(true);
-    
-        // adding a collider between player and button for (testing)
-        this.physics.add.collider(this.player, this.wall);
-        this.physics.add.overlap(this.player, this.chest, function(player, chest) { goldPickupAudio.play(); chest.destroy(); }, null, this);
-        
-        // create bindings to the arrow keys
-        this.cursors = this.input.keyboard.createCursorKeys();
-    }
-
-    update () {
-        this.player.setVelocity(0);
-    
-        if (this.cursors.left.isDown) {
-            this.player.setVelocityX(-160);
-        } else if (this.cursors.right.isDown) {
-            this.player.setVelocityX(160);
-        } 
-    
-        if (this.cursors.up.isDown) {
-            this.player.setVelocityY(-160);
-        } else if (this.cursors.down.isDown) {
-            this.player.setVelocityY(160);
-        }
+    create() {
+        this.scene.start('Game');
     }
 }
