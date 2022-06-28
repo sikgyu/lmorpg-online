de07ae15478f48daf3d8394ffa685a7233bae291
sikgyu <57619483+sikgyu@users.noreply.github.com>
2022-06-28 11:50:53 -0700

    :art: Implement UI Button Class

--- assets/js/scenes/TitleScene.js
+++ assets/js/scenes/TitleScene.js
@@ -8,20 +8,20 @@ class TitleScene extends Phaser.Scene {
         // create title text
         this.titleText = this.add.text(this.scale.width / 2 , this.scale.height / 2 , 'Little Multiplayer Online RPG', { fontSize: '40px', fill: '#fff', fontFamily: 'Arial' });
         this.titleText.setOrigin(0.5);
-        this.button = this.add.image(this.scale.width / 2 , this.scale.height * 0.65, 'button1');
-        this.button.setInteractive();
-        // create a Button
-        this.buttonText = this.add.text(0, 0, 'Start', { fontSize: '26px', fill: '#fff', fontFamily: 'Arial' });
-        Phaser.Display.Align.In.Center(this.buttonText, this.button);
 
-        this.button.on('pointerdown', () => {
-            this.scene.start('Game');
-        })
-        this.button.on('pointerover', () => {
-            this.button.setTexture('button2');
-        })
-        this.button.on('pointerout', () => {
-            this.button.setTexture('button1');
-        })
+        // create a Play Button
+        this.startGameButton = new UiButton(this, this.scale.width / 2 , this.scale.height * 0.65, 'button1', 'button2', 'Start', this.startScene.bind(this, 'Game'));
+        
+        // create a Button again
+        this.AnotherButton = new UiButton(this, this.scale.width / 2 , this.scale.height * 0.75, 'button1', 'button2', 'Another Button', this.emailMe.bind(this, 'dlatlrrb@gmail.com'));
+
+    }
+
+    startScene(targetScene) {
+        this.scene.start(targetScene);
+    }
+
+    emailMe(target) {
+        alert(target);
     }
 }
