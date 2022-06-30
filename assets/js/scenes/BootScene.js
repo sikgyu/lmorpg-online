f44205fb8b04c86b89a3d5dafbf1af80c145185f
sikgyu <57619483+sikgyu@users.noreply.github.com>
2022-06-30 14:07:53 -0700

    :bulb: Add comment

--- assets/js/scenes/BootScene.js
+++ assets/js/scenes/BootScene.js
@@ -5,10 +5,25 @@ class BootScene extends Phaser.Scene {
     }
 
     preload() {
+        // load images
+        this.loadImages();
+        // load spritesheets
+        this.loadSpriteSheets();
+        // load audio
+        this.loadAudio();
+    }
+
+    loadImages() {
         this.load.image('button1', 'assets/images/ui/blue_button01.png');
         this.load.image('button2', 'assets/images/ui/blue_button02.png');
+    }
+
+    loadSpriteSheets() {
         this.load.spritesheet('items', 'assets/images/items.png', { frameWidth: 32, frameHeight: 32 });
         this.load.spritesheet('characters', 'assets/images/characters.png', { frameWidth: 32, frameHeight: 32 });
+    }
+
+    loadAudio() {
         this.load.audio('goldsound', 'assets/audio/Pickup.wav')
     }
 
